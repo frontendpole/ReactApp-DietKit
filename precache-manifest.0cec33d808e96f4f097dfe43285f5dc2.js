1c29a72aec66e166ce58f4720bf3d46fdd56650a
Ola <aleksandra.karp1@gmail.com>
2020-09-27 17:51:57 +0200

    Updates

---
+++ precache-manifest.0cec33d808e96f4f097dfe43285f5dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71f7ddc96b74e8a73f76aaeb954b523",
+    "url": "/frontendpole.github.io/react-app-easy-diet-app/index.html"
+  },
+  {
+    "revision": "b07731490ef6cc39c5ca",
+    "url": "/frontendpole.github.io/react-app-easy-diet-app/static/css/main.a10cf371.chunk.css"
+  },
+  {
+    "revision": "c09cbf68df6392382052",
+    "url": "/frontendpole.github.io/react-app-easy-diet-app/static/js/2.2f4a89fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontendpole.github.io/react-app-easy-diet-app/static/js/2.2f4a89fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07731490ef6cc39c5ca",
+    "url": "/frontendpole.github.io/react-app-easy-diet-app/static/js/main.9e493ef5.chunk.js"
+  },
+  {
+    "revision": "3058b6775100eb780e3d",
+    "url": "/frontendpole.github.io/react-app-easy-diet-app/static/js/runtime-main.ea8e8337.js"
+  }
+]);
