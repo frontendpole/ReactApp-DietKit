cbde2c7025cb88f8ead6bbd288c9bd8b10c16cd7
Ola <aleksandra.karp1@gmail.com>
2020-09-27 17:19:19 +0200

    Updates

---
+++ precache-manifest.ddfbc1642dd9206d364d2da26ac1f7b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8c4c40539330734e8216e088e30510",
+    "url": "/ReactApp-Easy-Diet-App/index.html"
+  },
+  {
+    "revision": "0bb4f872198682704837",
+    "url": "/ReactApp-Easy-Diet-App/static/css/main.a10cf371.chunk.css"
+  },
+  {
+    "revision": "c09cbf68df6392382052",
+    "url": "/ReactApp-Easy-Diet-App/static/js/2.2f4a89fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp-Easy-Diet-App/static/js/2.2f4a89fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb4f872198682704837",
+    "url": "/ReactApp-Easy-Diet-App/static/js/main.a5a5fe61.chunk.js"
+  },
+  {
+    "revision": "97457d728940caaa9279",
+    "url": "/ReactApp-Easy-Diet-App/static/js/runtime-main.0d83ba7c.js"
+  }
+]);
